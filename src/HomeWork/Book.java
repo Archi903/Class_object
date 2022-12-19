0e6745848f033a42105fd6e0398c12ab3842675e
Archi903 <artpele@yandex.ru>
2022-12-19 16:43:39 +0300

    HomeWork

---
+++ src/HomeWork/Book.java
@@ -0,0 +1,24 @@
+package HomeWork;
+
+public class Book {
+    private String nameBook;
+    private int age;
+
+
+    public Book(String nameBook, int age) {
+        this.nameBook = nameBook;
+        this.age = age;
+    }
+
+    public String getNameBook() {
+        return this.nameBook;
+    }
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
---
+++ src/HomeWork/Main.java
@@ -0,0 +1,21 @@
+package HomeWork;
+
+public class Main {
+    public static void main(String[] args) {
+        Author author1 = new Author("Кэти", "Сьерра");
+        Book book1 = new Book("Java", 2012);
+        System.out.println("author1.firstName = " + author1.getFirstName());
+        System.out.println("author1.secondName = " + author1.getSecondName());
+        System.out.println("book1.nameBook = " + book1.getNameBook());
+        System.out.println("book1.age = " + book1.getAge());
+
+
+        book1.setAge(2002);
+        System.out.println("book1 = " + book1.getAge());
+
+        Author author2 = new Author("Лев", "Толстой");
+        Book book2 = new Book("Война и мир", 1873);
+        System.out.println("Автор книги №2 = " + author2.getFirstName() + " " + author2.getSecondName());
+        System.out.println("Название и год книги №2 = " + book2.getNameBook() + " " + book2.getAge());
+    }
+}
